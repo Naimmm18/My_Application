07a256a44540e54f3e900a05de84d2c631ab2eff
User <a.naim1806gmail.com>
2022-10-21 00:52:10 +0800

    activity1&dua

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,10 +14,14 @@ public class MainActivity extends AppCompatActivity {
     Button btnName;
     EditText etBirth;
     EditText etPhone;
+    EditText etAddress;
+    EditText etEmail;
 
     public final static String MESSAGE_KEY = "com.example.myapplication.message_key";
     public final static String BIRTH_KEY = "com.example.myapplication.birth_key";
     public final static String PHONE_KEY = "com.example.myapplication.phone_key";
+    public final static String ADDRESS_KEY = "com.example.myapplication.address_key";
+    public final static String EMAIL_KEY = "com.example.myapplication.email_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +33,21 @@ public class MainActivity extends AppCompatActivity {
         btnName = findViewById(R.id.btnName);
         etBirth = findViewById(R.id.etBirth);
         etPhone = findViewById(R.id.etPhone);
+        etAddress = findViewById(R.id.etAddress);
+        etEmail = findViewById(R.id.etEmail);
 
         String message = etName.getText().toString();
         String birthtxt = etBirth.getText().toString();
         String phonetxt = etPhone.getText().toString();
+        String addresstxt = etAddress.getText().toString();
+        String emailtxt = etEmail.getText().toString();
 
         Intent intent = new Intent(MainActivity.this,SecondActivity.class);
         intent.putExtra(MESSAGE_KEY,message);
         intent.putExtra(BIRTH_KEY,birthtxt);
         intent.putExtra(PHONE_KEY,phonetxt);
+        intent.putExtra(ADDRESS_KEY,addresstxt);
+        intent.putExtra(EMAIL_KEY,emailtxt);
 
         startActivity(intent);
 
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -11,6 +11,8 @@ public class SecondActivity extends AppCompatActivity {
     public final static String MESSAGE_KEY = "com.example.myapplication.message_key";
     public final static String BIRTH_KEY = "com.example.myapplication.birth_key";
     public final static String PHONE_KEY = "com.example.myapplication.phone_key";
+    public final static String ADDRESS_KEY = "com.example.myapplication.address_key";
+    public final static String EMAIL_KEY = "com.example.myapplication.email_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,12 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MESSAGE_KEY);
         String birthtxt = intent.getStringExtra(BIRTH_KEY);
         String phonetxt = intent.getStringExtra(PHONE_KEY);
+        String addresstxt = intent.getStringExtra(ADDRESS_KEY);
+        String emailtxt = intent.getStringExtra(EMAIL_KEY);
+
         TextView textview = new TextView(this);
-        textview.setTextSize(45);
+        textview.setTextSize(30);
+        textview.setText("NAME : "+message+"\n"+"BIRTHDAY : "+birthtxt+"\n"+"NO.PHONE : "+phonetxt+"\n"+" : "+addresstxt+"\n"+"EMAIL : "+emailtxt);
         setContentView(textview);
 
     }
